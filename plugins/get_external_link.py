3f4c2b2836bc8c953f7e383d2dde1c84afa30253
ssoorajr <50820025+ssoorajr@users.noreply.github.com>
2019-07-20 13:17:42 +0530

    Add files via upload

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -14,7 +14,6 @@ import requests
 import subprocess
 import time
 import json
-import re
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -83,37 +82,15 @@ def get_link(bot, update):
             adfulurl = file_inance.webContentLink
             max_days = 0
         else:
-            url = "https://up.uploadfiles.io/upload"
-            max_days = 5
-            
+            url = "https://api.anonymousfiles.io/"
+            max_days = 3
             command_to_exec = [
                 "curl",
-                "-X", "POST",
-                "-d", "@"+after_download_file_name,
-                "-H", "Filename: "+after_download_file_name,
+                "-F", "file=@"+after_download_file_name,
+                "-F", "expires_at=3d",
+                "-F", "no_index=true",
                 url
             ]
-            
-            # Example of t_response
-            # {
-                # "filename": "Hell.srt",
-                # "bin": "s64jjtgagpgevz8h",
-                # "bytes": 96237,
-                # "mime": "text/plain; charset=utf-8",
-                # "created": "2019-07-18T10:13:20.677271056Z",
-                # "links": [
-                    # {
-                        # "rel": "file",
-                        # "href": "https://filebin.net/s64jjtghgpgevz8h/Hellboy.srt"
-                    # },
-                    # {
-                        # "rel": "bin",
-                        # "href": "https://filebin.net/s64jjtghgpgevz8h"
-                    # }
-                # ],
-                # "datetime": "0001-01-01T00:00:00Z"
-            # }
-                
             bot.edit_message_text(
                 text=Translation.UPLOAD_START,
                 chat_id=update.chat.id,
@@ -133,15 +110,13 @@ def get_link(bot, update):
             else:
                 logger.info(t_response)
                 print ( t_response )
-                # t_response_arry = json.loads(t_response.decode("UTF-8").split("\n")[-1].strip())['url']
-                
-                scan = re.search(r'.*\"file\"\,[\s\n]*"slug":\s*\"(.+)\"', t_response.decode("UTF-8"))
+                t_response_arry = json.loads(t_response.decode("UTF-8").split("\n")[-1].strip())['url']
                 
                 #shorten_api_url = "http://ouo.io/api/{}?s={}".format(Config.OUO_IO_API_KEY, t_response_arry)
                 #adfulurl = requests.get(shorten_api_url).text
         bot.edit_message_text(
             chat_id=update.chat.id,
-            text=Translation.AFTER_GET_DL_LINK.format(scan.group(1), max_days),
+            text=Translation.AFTER_GET_DL_LINK.format(t_response_arry, max_days),
             parse_mode=pyrogram.ParseMode.HTML,
             message_id=a.message_id,
             disable_web_page_preview=True
