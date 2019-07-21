1d59bc7ffaaa622955bc821966840c397cefb740
ssoorajr <50820025+ssoorajr@users.noreply.github.com>
2019-07-21 15:45:44 +0530

    Update and rename get_external_link.py to anonymousfiles.py

--- plugins/anonymousfiles.py
+++ plugins/anonymousfiles.py
@@ -32,9 +32,9 @@ from helper_funcs.display_progress import progress_for_pyrogram
 from pydrive.drive import GoogleDrive
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["getlink"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["anonymousfiles"]))
 def get_link(bot, update):
-    TRChatBase(update.from_user.id, update.text, "getlink")
+    TRChatBase(update.from_user.id, update.text, "anonymousfiles")
     if str(update.from_user.id) in Config.BANNED_USERS:
         bot.send_message(
             chat_id=update.chat.id,
