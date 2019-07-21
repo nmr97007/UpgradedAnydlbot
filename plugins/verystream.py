c6ab5ef40c1f5b63f4edeed7f41c5c154a7defb6
ssoorajr <50820025+ssoorajr@users.noreply.github.com>
2019-07-22 00:32:32 +0530

    Update verystream.py

--- plugins/verystream.py
+++ plugins/verystream.py
@@ -83,7 +83,7 @@ def get_link(bot, update):
             adfulurl = file_inance.webContentLink
             max_days = 0
         else:
-            url = "https://ohsomp4wow.verystream.net/uploadproxy/k8uW0ztncF0aeIe3"
+            url = "https://ohsomp4wow.verystream.net/"
             max_days = 5
             
             command_to_exec = [
