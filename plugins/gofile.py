3686451e46eec792fe758e059995dcbe67201263
ssoorajr <50820025+ssoorajr@users.noreply.github.com>
2019-07-24 22:49:31 +0530

    Update gofile.py

--- plugins/gofile.py
+++ plugins/gofile.py
@@ -83,7 +83,9 @@ def get_link(bot, update):
             logger.info(file_inance)
             adfulurl = file_inance.webContentLink
             max_days = 0
-        else:
+        else: 
+            str(update.from_user.id) in Config.G_DRIVE_AUTH_DRQ:
+            gauth = Config.G_DRIVE_AUTH_DRQ[str(update.from_user.id)]
             url = "https://srv-file5.gofile.io/upload"
             max_days = 5
             timeseconds = int(time.time())
